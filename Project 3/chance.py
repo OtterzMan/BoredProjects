c2b40020cf3676014c3030541fab930728f1248b
OtterzMan <92121228+OtterzMan@users.noreply.github.com>
2022-02-18 01:08:16 +0000

    Add files via upload

---
+++ Project 3/chance.py
@@ -0,0 +1,46 @@
+import time
+import click
+import webbrowser
+
+
+output = input("Chose a number hehe:")
+output = int(output)
+time.sleep(2)
+
+if output == 1:
+    print("you are safe today hahaha")
+
+elif output == 2:
+    print("die")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+elif output == 3:
+    print("you are safe today hahaha")
+    
+
+elif output == 4:
+    print("you are safe today hahaha")
+    
+
+elif output == 5:
+    print("you are safe today hahaha")
+    
---
+++ Project 4/JoesDad.py
@@ -0,0 +1,8 @@
+joesdad = 23
+if joesdad == 24:
+    ("he came back !!!")
+    input(" ")
+
+else:
+    print("unlcky bro wait another 10 mins...")
+    input(" ")
